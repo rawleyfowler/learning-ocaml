e191b7b21b0598d411e37d8dddfb1ebeb5dc07ac
RawleyFowler <rawleyfowler@gmail.com>
2022-07-31 18:00:00 -0600

    add fold and tail recursion

--- fold.ml
+++ fold.ml
@@ -22,3 +22,24 @@ let join' lst ~sep:a =
   combine "" lst (fun v l -> match l with
   | "" -> (a ^ v)
   | _ -> (l ^ a ^ v));;
+
+(* Fold right, this implementation is not tail recursive, see fold_right_tr *)
+let rec fold_right f lst acc =
+  match lst with
+  | [] -> acc
+  | h :: t -> f h (fold_right f t acc);;
+
+(* This is tail recursive because we perform the work before the recursion  *)
+let rec fold_right_tr f lst acc =
+  match lst with
+  | [] -> acc
+  | h :: t ->
+      let acc' = f acc h in
+      fold_right_tr f t acc';;
+
+let rec reverse lst =
+  let rec reverse' lst' acc =
+    match lst' with
+    | [] -> acc
+    | h :: t -> reverse' t (h :: acc)
+  in reverse' lst [];;
